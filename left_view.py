ab022144ee1e6af6ad1025cd007288aeaf2ab836
Furkan Karakas <furkan.karakas@epfl.ch>
2022-01-23 12:49:35 +0100

    left view of a binary tree

---
+++ left_view.py
@@ -0,0 +1,56 @@
+from typing import Dict
+
+
+class Node:
+    def __init__(self, value: int) -> None:
+        self.left = None
+        self.right = None
+        self.value = value
+
+
+def tree_height(root: Node) -> int:
+    """Calculate the height of the tree whose root is `root`
+    """
+
+    if root is None:
+        return -1
+    return 1 + max(tree_height(root.left), tree_height(root.right))
+
+
+def left_view(root: Node, current_height: int, height_dict: Dict[str, int]) -> None:
+    """Print the left view of a binary tree
+
+    Args:
+        root (Node): Root node
+        current_height (int): Current height in the tree
+        height_dict (dict): Dictionary storing "height" and "last_height"
+    """
+
+    if root is None:
+        return
+    # If the last printed height is "gtoet" height of tree, simply return
+    if height_dict["last_height"] >= height_dict["height"]:
+        return
+    # We have the desired element here
+    if current_height > height_dict["last_height"]:
+        height_dict["last_height"] += 1
+        print(root.value)
+    # Recursively trace the left and right subtrees. Note that left child is called first
+    left_view(root.left, current_height+1, height_dict)
+    left_view(root.right, current_height+1, height_dict)
+
+
+if __name__ == "__main__":
+    # height 0
+    tree = Node(10)
+    # height 1
+    tree.left, tree.right = Node(6), Node(5)
+    # height 2
+    tree.left.right, tree.right.left, tree.right.right = Node(
+        3), Node(7), Node(8)
+    height = tree_height(tree)
+    d = dict({"last_height": -1,
+              "height": height})
+    print(f"The height of the tree: {height}")
+    print(f"Left view of the tree:")
+    left_view(tree, 0, d)
