a565e147ae4d2f10f13c03aced638a4a37373882
Furkan Karakas <furkan.karakas@epfl.ch>
2023-12-26 17:27:01 +0100

    Optimal solution added

--- out_of_boundary_paths.py
+++ out_of_boundary_paths.py
@@ -2,60 +2,57 @@
 There is an m x n grid with a ball. The ball is initially at the position [startRow, startColumn]. You are allowed to move the ball to one of the four adjacent cells in the grid (possibly out of the grid crossing the grid boundary). You can apply at most maxMove moves to the ball.
 
 Given the five integers m, n, maxMove, startRow, startColumn, return the number of paths to move the ball out of the grid boundary. Since the answer can be very large, return it modulo 1e9 + 7.
-"""
 
-from collections import deque
+Note that we can solve this problem with BFS, but it takes a lot of memory space (the queue gets too large).
+
+We can also solve this problem with DFS, but it takes a lot of time and recursive callstack.
+
+We will solve it with dynamic programming and storing the positions in the current and previous steps.
+"""
 
 
 def findPaths(m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
-    def cell_contribution(x: int, y: int) -> int:
-        if x == y == 0 and m == n == 1:
-            return 4
-        if (x in (0, m-1) and y in (0, n-1)) and 1 in (m, n):
-            return 3
-        if (x in (0, m-1) or y in (0, n-1)) and 1 in (m, n):
-            return 2
-        if x in (0, m-1) and y in (0, n-1):
-            return 2
-        if x in (0, m-1) or y in (0, n-1):
-            return 1
-        return 0
+    def is_inbound(x: int, y: int) -> int:
+        if x in (-1, m) or y in (-1, n):
+            return False
+        return True
 
-    # dp=[[0]*n for _ in range(m)]
-    q = deque()
-    q.append((startRow, startColumn))
-    currentMove = 0
+    # dp stores the number of possible ways to reach that cell at current move
+    dp = [[0]*n for _ in range(m)]
+    # Initialize it with 1 at the start position
+    dp[startRow][startColumn] = 1
     result = 0
+    # Store the directions the ball can move
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-    while len(q) > 0:
-        if currentMove >= maxMove:
-            break
+    for _ in range(maxMove):
+        # Store the current configuration in a temporary matrix
+        temp = [[0]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                # If there are no moves from the previous iteration, skip
+                if dp[i][j] == 0:
+                    continue
 
-        currentMove += 1
-        qLen = len(q)
-        # print(qLen)
-        for i in range(qLen):
-            x, y = q.popleft()
-            result += cell_contribution(x, y)
-            # Add up to queue
-            if x-1 >= 0:
-                q.append((x-1, y))
-            # Add down to queue
-            if x+1 <= m-1:
-                q.append((x+1, y))
-            # Add left to queue
-            if y-1 >= 0:
-                q.append((x, y-1))
-            # Add right to queue
-            if y+1 <= n-1:
-                q.append((x, y+1))
+                # Move in each direction
+                for direction_x, direction_y in directions:
+                    x = i+direction_x
+                    y = j+direction_y
+
+                    if is_inbound(x, y):
+                        # Add it to the possible moves from previous position
+                        temp[x][y] += dp[i][j]
+                    else:
+                        # Add it to result set
+                        result += dp[i][j]
+
+        # Update dp with the current possible moves
+        dp = temp
 
     return result % int(1e9 + 7)
 
 
 if __name__ == "__main__":
-    m = n = 1
-    maxMove = 4
-    startRow = startColumn = 0
+    m, n, maxMove, startRow, startColumn = 5, 5, 3, 2, 2
     result = findPaths(m, n, maxMove, startRow, startColumn)
-    print(f"Result: {result}")
+    print(result)
