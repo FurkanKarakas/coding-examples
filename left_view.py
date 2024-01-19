5dd8f414413c8e62510d3ef5fcd00994f72cc2c4
Furkan Karakas <furkan.karakas@epfl.ch>
2024-01-19 13:06:40 +0100

    update

--- left_view.py
+++ left_view.py
@@ -5,36 +5,24 @@ class Node:
         self.value: int = value
 
 
-def tree_height(root: Node | None) -> int:
-    """Calculate the height of the tree whose root is `root`
-    """
-
-    if root is None:
-        return -1
-    return 1 + max(tree_height(root.left), tree_height(root.right))
-
-
-def left_view(root: Node | None, current_height: int, height_dict: dict[str, int]) -> None:
+def left_view(root: Node | None, current_height: int, max_height: list[int]) -> None:
     """Print the left view of a binary tree
 
     Args:
         root (Node): Root node
         current_height (int): Current height in the tree
-        height_dict (dict): Dictionary storing "height" and "last_height"
     """
 
     if root is None:
         return
-    # If the last printed height is "gtoet" height of tree, simply return
-    if height_dict["last_height"] >= height_dict["height"]:
-        return
-    # We have the desired element here
-    if current_height > height_dict["last_height"]:
-        height_dict["last_height"] += 1
+
+    if current_height == max_height[0]:
+        max_height[0] += 1
         print(root.value)
+
     # Recursively trace the left and right subtrees. Note that left child is called first
-    left_view(root.left, current_height+1, height_dict)
-    left_view(root.right, current_height+1, height_dict)
+    left_view(root.left, current_height+1, max_height)
+    left_view(root.right, current_height+1, max_height)
 
 
 if __name__ == "__main__":
@@ -45,9 +33,6 @@ if __name__ == "__main__":
     # height 2
     tree.left.right, tree.right.left, tree.right.right = Node(
         3), Node(7), Node(8)
-    height = tree_height(tree)
-    d = dict({"last_height": -1,
-              "height": height})
-    print(f"The height of the tree: {height}")
+
     print(f"Left view of the tree:")
-    left_view(tree, 0, d)
+    left_view(tree, 0, [0])
