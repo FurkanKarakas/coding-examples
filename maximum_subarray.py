43975e0e6c77acff0e63adc3b4c7dd330216b476
Furkan Karakas <furkan.karakas@epfl.ch>
2024-02-22 21:45:22 +0100

    maximum subarray sum using dynamic programming with memory optimization

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,39 +1,44 @@
-from typing import List, Tuple
+"""https://leetcode.com/problems/maximum-subarray/description/
 
 
-def kadene_algorithm(arr: List[int]) -> Tuple[int, int, int]:
-    """This function implements the Kadene's algorithm which finds the maximum subarray of a given array.
+Given an array of integers, return the maximum subarray sum, start and end indices.
 
-    Args:
-        arr (List[int]): This is a list of positive and negative integers. At least one integer must be non-negative.
+We can solve this problem using dynamic programming by observing the following substructure:
 
-    Returns:
-        Tuple[int, int, int]: Returns the largest sum, start index of the subarray and the end index of the subarray inclusive.
-    """
+dp[i] = max(dp[i-1]+arr[i], arr[i])
 
-    # current_sum variable stores the sum ending in index i and starting in a non-negative sum index (start_index_temp).
-    # best_sum variable stores the best sum seen so far.
-    start_index_temp = start_index = end_index = best_sum = current_sum = 0
-    for i, x in enumerate(arr):
-        current_sum = current_sum+x
-        # If current_sum is negative, reset the potential start index.
-        # We basically go bankrupt: start from a potentially new start index :)
-        if current_sum < 0:
-            current_sum = 0
-            # The next index after the current index that we are at.
-            start_index_temp = i+1
-        if best_sum >= current_sum:
-            pass
-        # Adapt the new suggested starting index from the current sum and the last index
+We can optimize the dynamic programming approach by just taking the previous item in the dp array.
+"""
+
+
+def maximum_subarray(arr: list[int]) -> tuple[int, int, int]:
+    result = -abs(arr[0])-1
+    dp_prev = -abs(arr[0])-1
+    start = 0
+    end = 0
+
+    for i, item in enumerate(arr):
+        temp1 = dp_prev+item
+        temp2 = max(temp1, item, result)
+
+        if temp2 == temp1:
+            # Adopt the previous start index
+            end = i
+        elif temp2 == item:
+            # Both indices start here
+            start = end = i
         else:
-            best_sum = current_sum
-            end_index = i
-            start_index = start_index_temp
-    return best_sum, start_index, end_index
+            # Maximum element didn't change, don't change
+            pass
+
+        dp_prev = max(temp1, item)
+        result = temp2
+
+    return result, start, end
 
 
 if __name__ == "__main__":
     arr = [-2, -3, 4, -1, -2, 1, 5, -3]
-    best_sum, start_index, end_index = kadene_algorithm(arr)
-    print(f"Array: {arr}", f"Best sum: {best_sum}", f"Start index: {start_index}",
-          f"End index: {end_index}", f"Maximum subarray: {arr[start_index:end_index+1]}", sep="\n")
+    result, start, end = maximum_subarray(arr)
+    print(f"Array: {arr}\nMaximum subarray sum: {
+          result}\nStart index: {start}\nEnd index: {end}")
